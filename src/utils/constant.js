9ae0818a092d3259b791755c9e0d28158c0f43ef
deepakmeena575 <147081660+deepakmeena575@users.noreply.github.com>
2025-06-21 10:23:28 +0530

    const changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://8z9sb516-8000.inc1.devtunnels.ms/api/v1/user";
-export const JOB_API_END_POINT="https://8z9sb516-8000.inc1.devtunnels.ms/api/v1/job";
-export const APPLICATION_API_END_POINT="https://8z9sb516-8000.inc1.devtunnels.ms/api/v1/application";
-export const COMPANY_API_END_POINT="https://8z9sb516-8000.inc1.devtunnels.ms/api/v1/company";
+export const USER_API_END_POINT="https://project-frontend-psi-green.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="https://project-frontend-psi-green.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://project-frontend-psi-green.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://project-frontend-psi-green.vercel.app/api/v1/company";
